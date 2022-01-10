c8fd71c4ec1d68b41b13a6c6bbe2ff487f530787
matthis-k <matthis.kaelble@gmail.com>
2022-01-11 00:52:08 +0100

    adjust liveinstall

--- liveinstall.sh
+++ liveinstall.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
+
+DISK=vda
+ROOT=vda2
+EFI=vda1
+
 loadkeys de-latin1
-ls /sys/firmware/efi/efivars
+IS_UEFI=0
+ls /sys/firmware/efi/efivars && IS_UEFI=1
 timedatectl set-ntp true
-cfdisk /dev/disk
-mkfs.ext4 /dev/root_partition
-mkfs.fat -F 32 /dev/efi_system_partition
-mount /dev/root_partition /mnt
+cfdisk /dev/$DISK
+mkfs.ext4 /dev/$ROOT
+mkfs.fat -F 32 /dev/$EFI
+mount /dev/$ROOT /mnt
 mkdir /mnt/boot
-mount /dev/efi_system_partition /mnt/boot
+mount /dev/$EFI /mnt/boot
 pacstrap /mnt base linux linux-firmware base-devel vi vim rustup git iwd man-pages man-db texinfo
 genfstab -U /mnt >> /mnt/etc/fstab
+[ $IS_UEFI == 1 ] && echo Note: this is an uefi system || echo Note: this is no uefi system
+
+read you will chroot onto the install
 arch-chroot /mnt
 
