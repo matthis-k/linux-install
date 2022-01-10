2b7d66d491044ee7d70674d947a5f666e76549c8
matthis-k <matthis.kaelble@gmail.com>
2022-01-10 23:08:59 +0100

    add setup script

---
+++ install.sh
@@ -0,0 +1,11 @@
+ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+hwclock --systohc
+sed 's/#\(de_DE.UTF-8\|en_US.UTF-8\)/\1/g' > locale.gen
+locale-gen
+echo LANG=en_US.UTF-8 > /etc/locale.conf
+KEYMAP=de-latin1 > /etc/vconsole.conf
+echo myhostname > /etc/hostname
+mkinitcpio -P
+passwd
+grub-install --target=i386-pc /dev/vda
+grub-mkconfig -o /boot/grub/grub.cfg
