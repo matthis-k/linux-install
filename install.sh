f2b5b84342108e6a891469ba4ac4cb5fd21dd297
matthis-k <matthis.kaelble@gmail.com>
2022-01-11 23:18:23 +0100

    add to install script

--- install.sh
+++ install.sh
@@ -9,3 +9,18 @@ mkinitcpio -P
 passwd
 grub-install --target=i386-pc /dev/vda
 grub-mkconfig -o /boot/grub/grub.cfg
+sudo pacman -Syy rustup base-devel git
+rustup toolchain add nightly && rustup toolchain add stable && rustup default nightly
+git clone https://github.com/matthis-k/config
+git clone https://github.com/matthis-k/config-manager
+(
+    cd config-manager/
+    sudo ./deploy-config  ~/config
+    ./install-needed-packags ~/config
+)
+sudo systemctl enable sddm
+sudo systemctl enable preload
+sudo systemctl enable iwd
+sudo systemctl enable bluetooth
+chsh -s /bin/fish
+sudo chsh -s /bin/fish
