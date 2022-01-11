8367c9f27c16488a5cf0ba9fdbab3973ac586378
matthis-k <matthis.kaelble@gmail.com>
2022-01-11 01:04:45 +0100

    fis: wring sed

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
 hwclock --systohc
-sed 's/#\(de_DE.UTF-8\|en_US.UTF-8\)/\1/g' > locale.gen
+sed 's/#\(de_DE.UTF-8\|en_US.UTF-8\)/\1/g' /etc/locale.gen > /etc/locale.gen
 locale-gen
 echo LANG=en_US.UTF-8 > /etc/locale.conf
 KEYMAP=de-latin1 > /etc/vconsole.conf
