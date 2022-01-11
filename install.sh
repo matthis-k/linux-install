17916dd5e8924c62cf0fd8776b6d6611cbec4955
matthis-k <matthis.kaelble@gmail.com>
2022-01-11 23:26:33 +0100

    add sudos

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
-ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
-hwclock --systohc
-sed 's/#\(de_DE.UTF-8\|en_US.UTF-8\)/\1/g' /etc/locale.gen > /etc/locale.gen
-locale-gen
-echo LANG=en_US.UTF-8 > /etc/locale.conf
-KEYMAP=de-latin1 > /etc/vconsole.conf
-echo myhostname > /etc/hostname
-mkinitcpio -P
+sudo ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+sudo hwclock --systohc
+sudo sed 's/#\(de_DE.UTF-8\|en_US.UTF-8\)/\1/g' /etc/locale.gen > /etc/locale.gen
+sudo locale-gen
+sudo echo LANG=en_US.UTF-8 > /etc/locale.conf
+sudo KEYMAP=de-latin1 > /etc/vconsole.conf
+sudo echo myhostname > /etc/hostname
+sudo mkinitcpio -P
 passwd
-grub-install --target=i386-pc /dev/vda
-grub-mkconfig -o /boot/grub/grub.cfg
+sudo grub-install --target=i386-pc /dev/vda
+sudo grub-mkconfig -o /boot/grub/grub.cfg
 sudo pacman -Syy rustup base-devel git
 rustup toolchain add nightly && rustup toolchain add stable && rustup default nightly
 git clone https://github.com/matthis-k/config
